09c06450f2af54afd98d4e31e0e67ee1856a3ed2
yp <473075889@qq.com>
2019-10-23 22:52:12 +0800

    Initial commit

---
+++ src/main/java/com/study/utils/excel/MainTest.java
@@ -0,0 +1,84 @@
+package com.study.utils.excel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Author: Dreamer-1
+ * Date: 2019-03-01
+ * Time: 10:13
+ * Description: 示例程序入口类
+ */
+public class MainTest {
+
+    private static Logger logger = Logger.getLogger(MainTest.class.getName());
+
+    public static void main(String[] args) {
+
+        /************** 读取Excel流程 ******************/
+        // 设定Excel文件所在路径
+        String excelFileName = "D:\\test.xls";
+        // 读取Excel文件内容
+        List<ExcelDataVO> readResult = ExcelReader.readExcel(excelFileName);
+        List<List<String>> readResultStr = ExcelReader.readExcelForStr(excelFileName);
+
+        // todo 进行业务操作
+
+        /************** 写入Excel流程 ******************/
+        // 创建需要写入的数据列表
+        List<ExcelDataVO> dataVOList = new ArrayList<>(2);
+        ExcelDataVO dataVO = new ExcelDataVO();
+        dataVO.setName("小明");
+        dataVO.setAge(18);
+        dataVO.setLocation("广州");
+        dataVO.setJob("大学生");
+        ExcelDataVO dataVO2 = new ExcelDataVO();
+        dataVO2.setName("小花");
+        dataVO2.setAge(19);
+        dataVO2.setLocation("深圳");
+        dataVO2.setJob("大学生");
+        dataVOList.add(dataVO);
+        dataVOList.add(dataVO2);
+
+        // 写入数据到工作簿对象内
+        Workbook workbook = ExcelWriter.exportData(dataVOList);
+
+        Workbook workbookStr = ExcelWriter.exportDataForStr(readResultStr);
+
+
+        // 以文件的形式输出工作簿对象
+        FileOutputStream fileOut = null;
+        try {
+            String exportFilePath = "C:\\Users\\Administrator\\Desktop\\test\\writeExample.xlsx";
+            File exportFile = new File(exportFilePath);
+            if (!exportFile.exists()) {
+                exportFile.createNewFile();
+            }
+
+            fileOut = new FileOutputStream(exportFilePath);
+            workbookStr.write(fileOut);
+            fileOut.flush();
+        } catch (Exception e) {
+            logger.warning("输出Excel时发生错误，错误原因：" + e.getMessage());
+        } finally {
+            try {
+                if (null != fileOut) {
+                    fileOut.close();
+                }
+                if (null != workbook) {
+                    workbook.close();
+                }
+            } catch (IOException e) {
+                logger.warning("关闭输出流时发生错误，错误原因：" + e.getMessage());
+            }
+        }
+
+    }
+
+}
---
+++ src/main/java/com/study/utils/excel/zhu.java
@@ -0,0 +1,115 @@
+package com.study.utils.excel;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
+
+public class zhu {
+
+    public static String point = "!@#$%^&*";
+
+    public static void main(String[] args) {
+        Map<String, List<List<String>>> map = new HashMap();
+        String path = "C:\\Users\\Administrator\\Desktop\\zwhtest"; //目录文件夹
+//        Set<String> dirFiles = new HashSet<>();
+
+        File file = new File(path);
+        File[] peopleFile = file.listFiles(); //每个人的文件夹
+        for (File peopleDir : peopleFile) {
+            String peopleName = peopleDir.getName();
+            File[] files = peopleDir.listFiles(); //每个人下面的文件夹
+            for (File dirFile : files) {
+                String dirFileName = dirFile.getName();  //每个具体的文件夹名
+//                dirFiles.add(dirFileName);
+                File[] excelFiles = dirFile.listFiles();
+                for (File targetFile : excelFiles) {
+                    String targetFileName = targetFile.getName();
+                    int i = targetFileName.lastIndexOf(".");
+                    targetFileName = targetFileName.substring(0, i);
+                    String key = dirFileName + point + targetFileName;
+                    if (map.get(key) == null) {
+                        map.put(key, new ArrayList<>());
+                    }
+                    String absolutePath = targetFile.getAbsolutePath();
+                    List<List<String>> lists = ExcelReader.readExcelForStr(absolutePath);
+                    map.get(key).addAll(lists);
+                }
+            }
+        }
+        Set<String> strings = map.keySet();
+
+        String outPath = "C:\\Users\\Administrator\\Desktop\\YP";  //输出的文件目录
+
+        for (String string : strings) {
+            List<List<String>> lists = map.get(string);
+            int i = string.indexOf(point);
+            String s = outPath + "\\" + string.substring(0, i) + "\\";
+            s = s + string.substring(i + point.length()) + ".xlsx";
+            export(lists, s);
+        }
+
+
+    }
+
+    public static void checkFile(File file) {
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                String parent = file.getParent();
+                File file1 = new File(parent);
+                checkDir(file1);
+                checkFile(file);
+            }
+        }
+    }
+
+    public static void checkDir(File file) {
+        if (!file.exists()) {
+            if (!file.mkdir()) {
+                String parent = file.getParent();
+                File file1 = new File(parent);
+                checkDir(file1);
+                checkDir(file);
+            }
+        } else {
+            if (file.isFile()) {
+                System.out.println("已有该文件，但不是文件夹:-->" + file.getName());
+                System.exit(0);
+            }
+        }
+    }
+
+
+    public static void export(List<List<String>> lists, String exportFilePath) {
+        Workbook workbookStr = ExcelWriter.exportDataForStr(lists);
+
+        // 以文件的形式输出工作簿对象
+        FileOutputStream fileOut = null;
+        try {
+            File exportFile = new File(exportFilePath);
+            checkFile(exportFile);
+
+            fileOut = new FileOutputStream(exportFilePath);
+            workbookStr.write(fileOut);
+            fileOut.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != fileOut) {
+                    fileOut.close();
+                }
+                if (null != workbookStr) {
+                    workbookStr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
